59337ae97b1fdae4c6e33aba9517b0c26defb488
abhis12321 <abhisheksingh2359328@gmail.com>
2024-07-04 09:42:03 +0530

    Form is created

--- app/_components/UploadForm.js
+++ app/_components/UploadForm.js
@@ -1,9 +1,19 @@
+"use client"
 import React from 'react'
 
-export default function UploadForm() {
-  return (
-    <div className=''>
-      new image upload form
-    </div>
-  )
+export default function ImageForm() {
+    const [images, setImage] = React.useState([]);
+    const handleSelect = e => {
+        let files = [...e.target.files];
+        let newfiles = files.filter(file => file.size <= 1025 * 1025);
+        setImage(prev => [...prev, ...newfiles]);
+    }
+
+    return (
+        <form className='flex flex-col gap-1 items-center justify-center rounded-md p-2 bg-red-300'>
+            <input type="file" accept="image/*" className="w-fit py8 max-fit h-fit text-red-950 text-center text-xs font-bold outline-none p-4" placeholder="" onChange={handleSelect} multiple required />
+
+
+        </form>
+    )
 }
--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import UploadForm from './_components/UploadForm'
 
 export default function page() {
   return (
     <div className=''>
+      <UploadForm />
       Image galary
     </div>
   )
