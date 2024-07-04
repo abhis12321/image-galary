43d5c40cb68650db63e4a77af14f8980468eebc3
abhis12321 <abhisheksingh2359328@gmail.com>
2024-07-04 09:16:05 +0530

    UploadForm component is created for uploading ne images

--- app/_components/UploadForm.js
+++ app/_components/UploadForm.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function page() {
+export default function UploadForm() {
   return (
     <div className=''>
-      Image galary
+      new image upload form
     </div>
   )
 }
